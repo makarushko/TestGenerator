356295b430bbb2175cc57a763a501470c625856c
Anna <makarushko7702@gmail.com>
2022-02-14 05:46:07 +0300

    added classInfo

---
+++ TestGeneratorLib/Info/ConstructorInfo.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+namespace TestGeneratorLib.Info
+{
+    public class ConstructorInfo
+    {
+            public string Name { get; private set; }
+            public Dictionary<string, string> Parameters { get; private set; }
+
+            public ConstructorInfo(Dictionary<string, string> parameters, string name)
+            {
+                this.Name = name;
+                this.Parameters = parameters;
+            }
+    }
+}
