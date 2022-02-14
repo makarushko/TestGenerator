9bd93cadd0ec4b221e535a563492c1518914bea4
Anna <makarushko7702@gmail.com>
2022-02-14 05:45:10 +0300

    get info about class

---
+++ TestGeneratorLib/Info/FileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TestGeneratorLib.Info
+{
+    public class FileInfo
+    {
+        public List<ClassInfo> Classes { get; private set; }
+
+        public FileInfo(List<ClassInfo> classes)
+        {
+            this.Classes = classes;
+        }
+    }
+}
